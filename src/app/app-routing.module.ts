ddf97cb308ca9d5e1ddfa381bf6c48da9172aa3b
gurezo <akihiko.kigure@gmail.com>
2019-04-19 19:01:28 +0900

    sample add.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: 'app/sample/sample.module#SampleModule'
+    loadChildren: './sample/sample.module#SampleModule'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/sample/sample.module.ts
+++ src/app/sample/sample.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SampleRoutingModule } from  './sample-routing.module';
 import { SampleComponent } from './sample.component';
 import { SampleService } from './sample.service';
 
@@ -8,7 +9,8 @@ import { SampleService } from './sample.service';
     SampleComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SampleRoutingModule
   ],
   providers: [
     SampleService
